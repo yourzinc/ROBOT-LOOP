9a7ff97d39cbd674f9b16526eebc232bf2cb4aa6
yourzinc <your_zinc@ewhain.net>
2022-09-29 14:00:31 +0900

    refactor: code refactor

--- ROBOT LOOP/sound.js
+++ ROBOT LOOP/sound.js
@@ -1,53 +1,79 @@
 // Sound Management
 
     Drum_key = [
-        { key : "81" , sound : new Audio('clap2.wav') }, //Q
-        { key : "87" , sound : new Audio('closedhat1.wav') }, //W
-        { key : "69" , sound : new Audio('closedhat2.wav') }, //E
-        { key : "82" , sound : new Audio('kicksnap.wav') }, //R
-        { key : "84" , sound : new Audio('openhat1.wav') }, //T
-        { key : "89" , sound : new Audio('openhat2.wav') }, //Y
-        { key : "85" , sound : new Audio('snappykick.wav') }, //U
-        { key : "73" , sound : new Audio('snaredrum1.wav') }, //I
-        { key : "79" , sound : new Audio('snaredrum2.wav') }, //O
-        { key : "80" , sound : new Audio('solidkick.wav') }  //P
+        { key : "81" , sound : new Audio("sound/clap2.wav") }, //Q
+        { key : "87" , sound : new Audio("sound/closedhat1.wav") }, //W
+        { key : "69" , sound : new Audio("sound/closedhat2.wav") }, //E
+        { key : "82" , sound : new Audio("sound/kicksnap.wav") }, //R
+        { key : "84" , sound : new Audio("sound/openhat1.wav") }, //T
+        { key : "89" , sound : new Audio("sound/openhat2.wav") }, //Y
+        { key : "85" , sound : new Audio("sound/snappykick.wav") }, //U
+        { key : "73" , sound : new Audio("sound/snaredrum1.wav") }, //I
+        { key : "79" , sound : new Audio("sound/snaredrum2.wav") }, //O
+        { key : "80" , sound : new Audio("sound/solidkick.wav") }  //P
+        ];
+    
+    Drum_key2 = [
+        { key : "81" , sound : new Audio("sound/clap2.wav") }, //Q
+        { key : "87" , sound : new Audio("sound/closedhat1.wav") }, //W
+        { key : "69" , sound : new Audio("sound/closedhat2.wav") }, //E
+        { key : "82" , sound : new Audio("sound/kicksnap.wav") }, //R
+        { key : "84" , sound : new Audio("sound/openhat1.wav") }, //T
+        { key : "89" , sound : new Audio("sound/openhat2.wav") }, //Y
+        { key : "85" , sound : new Audio("sound/snappykick.wav") }, //U
+        { key : "73" , sound : new Audio("sound/snaredrum1.wav") }, //I
+        { key : "79" , sound : new Audio("sound/snaredrum2.wav") }, //O
+        { key : "80" , sound : new Audio("sound/solidkick.wav") }  //P
         ];
 
     Drum_sound = [
-        'clap2.wav',
-        'closedhat1.wav',
-        'closedhat2.wav',
-        'kicksnap.wav',
-        'openhat1.wav',
-        'openhat2.wav',
-        'snappykick.wav',
-        'snaredrum1.wav',
-        'snaredrum2.wav',
-        'solidkick.wav'
+        "sound/clap2.wav",
+        "sound/closedhat1.wav",
+        "sound/closedhat2.wav",
+        "sound/kicksnap.wav",
+        "sound/openhat1.wav",
+        "sound/openhat2.wav",
+        "sound/snappykick.wav",
+        "sound/snaredrum1.wav",
+        "sound/snaredrum2.wav",
+        "sound/solidkick.wav"
     ]
 
     Robot_key = [
-        { key : "81" , sound : new Audio('r_level_a.wav') }, //Q
-        { key : "87" , sound : new Audio('r_try_again.wav') }, //W
-        { key : "69" , sound : new Audio('r_the_correct.wav') }, //E
-        { key : "82" , sound : new Audio('r_sayit.wav') }, //R
-        { key : "84" , sound : new Audio('r_rightnow.wav') }, //T
-        { key : "89" , sound : new Audio('r_check.wav') }, //Y
-        { key : "85" , sound : new Audio('r_all.wav') }, //U
-        { key : "73" , sound : new Audio('r_hello.wav') }, //I
-        { key : "79" , sound : new Audio('r_goodbye.wav') }, //O
-        { key : "80" , sound : new Audio('r_how_many.wav') }  //P
+        { key : "81" , sound : new Audio("sound/r_level_a.wav") }, //Q
+        { key : "87" , sound : new Audio("sound/r_try_again.wav") }, //W
+        { key : "69" , sound : new Audio("sound/r_the_correct.wav") }, //E
+        { key : "82" , sound : new Audio("sound/r_sayit.wav") }, //R
+        { key : "84" , sound : new Audio("sound/r_rightnow.wav") }, //T
+        { key : "89" , sound : new Audio("sound/r_check.wav") }, //Y
+        { key : "85" , sound : new Audio("sound/r_all.wav") }, //U
+        { key : "73" , sound : new Audio("sound/r_hello.wav") }, //I
+        { key : "79" , sound : new Audio("sound/r_goodbye.wav") }, //O
+        { key : "80" , sound : new Audio("sound/r_how_many.wav") }  //P
         ];
 
+    Robot_key2 = [
+        { key : "81" , sound : new Audio("sound/r_level_a.wav") }, //Q
+        { key : "87" , sound : new Audio("sound/r_try_again.wav") }, //W
+        { key : "69" , sound : new Audio("sound/r_the_correct.wav") }, //E
+        { key : "82" , sound : new Audio("sound/r_sayit.wav") }, //R
+        { key : "84" , sound : new Audio("sound/r_rightnow.wav") }, //T
+        { key : "89" , sound : new Audio("sound/r_check.wav") }, //Y
+        { key : "85" , sound : new Audio("sound/r_all.wav") }, //U
+        { key : "73" , sound : new Audio("sound/r_hello.wav") }, //I
+        { key : "79" , sound : new Audio("sound/r_goodbye.wav") }, //O
+        { key : "80" , sound : new Audio("sound/r_how_many.wav") }  //P
+        ];
+        
     Robot_sound = [
-        'r_level_a.wav',
-        'r_try_again.wav',
-        'r_the_correct.wav',
-        'r_sayit.wav',
-        'r_rightnow.wav',
-        'r_check.wav',
-        'r_all.wav',
-        'r_hello.wav',
-        'r_goodbye.wav',
-        'r_how_many.wav'
+        "sound/r_level_a.wav",
+        "sound/r_try_again.wav",
+        "sound/r_the_correct.wav",
+        "sound/r_sayit.wav",
+        "sound/r_rightnow.wav",
+        "sound/r_check.wav",
+        "sound/r_all.wav",
+        "sound/r_hello.wav",
+        "sound/r_goodbye.wav",
+        "sound/r_how_many.wav"
     ]
